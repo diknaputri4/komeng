96d3b227cd2ec2481f4ae35450be41f3e1ff8573
pandu <pandusubekti@gmail.com>
2024-01-09 20:54:30 +0700

    ya

---
+++ script.js
@@ -0,0 +1,19 @@
+// Toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+
+//ketika menu di klik
+document.querySelector('#menu'). onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+
+//klik diluar side bar untyk menghilangkan navbar
+const menu = document.querySelector('#menu');
+
+document.addEventListener('click', function(e) {
+  if(!menu.contains(e.target) && !navbarNav.contains(e.target)) 
+  {
+    navbarNav.classList.remove('active');
+
+  } 
+})
